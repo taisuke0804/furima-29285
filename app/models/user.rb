430a094dffc08d55412f279ff948a2c07b5672d9
taisuke0804 <ultrasoul1987@yahoo.co.jp>
2020-08-30 17:31:36 +0900

    ユーザーモデル、バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true, length: { maximum: 40 }
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kata, presence: true
-  validates :first_name_kata, presence: true
-  validates :birth, presence: true
+  
+  with_options presence: true do
+    validates :nickname, length: { maximum: 40 }
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :family_name_kata, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name_kata, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :birth
+  end
 end
