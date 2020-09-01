7d1733ba01936a370e488e60714fe6718085fc52
taisuke0804 <ultrasoul1987@yahoo.co.jp>
2020-09-01 19:08:50 +0900

    テストコード記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,19 +7,17 @@ class Item < ApplicationRecord
   belongs_to_active_hash :sending_day
 
   #空の投稿を保存できないようにする
-  validates :item, :image, :explanation, :price, presence: true
-  validates :category, presence: true
-  validates :condition, presence: true
-  validates :delivery_fee, presence: true
-  validates :sending_area, presence: true
-  validates :sending_day, presence: true
+  with_options presence: true do
+    validates :item, length: { maximum: 40 }
+    validates :explanation, length: { maximum: 1000 }
+    validates :price, presence: true, numericality: {greater_than: 299, less_than: 10000000}
+    validates :image
+  end
 
   #ジャンルの選択が「--」の時は保存できないようにする
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :delivery_fee_id, numericality: { other_than: 1 }
-  validates :sending_area_id, numericality: { other_than: 1 }
-  validates :sending_day_id, numericality: { other_than: 1 }
+  with_options numericality: { other_than: 1 } do
+    validates :category_id, :condition_id, :delivery_fee_id, :sending_area_id, :sending_day_id
+  end
   
   
   belongs_to :user
