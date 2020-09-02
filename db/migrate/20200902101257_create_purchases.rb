24e2189ca7e7d14801e04056b82dc44f14e2edd8
taisuke0804 <ultrasoul1987@yahoo.co.jp>
2020-09-02 19:24:32 +0900

    purchasesテーブル作成

--- db/migrate/20200902101257_create_purchases.rb
+++ db/migrate/20200902101257_create_purchases.rb
@@ -1,7 +1,13 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
-
+      t.string      :post_number,     null: false
+      t.integer     :prefecture,      null: false
+      t.string      :city,            null: false
+      t.string      :street_number,   null: false
+      t.string      :building
+      t.string      :phone_number,    null: false
+      t.references  :management,      null: false, foreign_key: true
       t.timestamps
     end
   end
