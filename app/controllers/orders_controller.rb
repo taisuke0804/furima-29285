b79c99a6729355b5826663941cadf2463bd86924
taisuke0804 <ultrasoul1987@yahoo.co.jp>
2020-09-03 17:16:32 +0900

    購入画面に商品情報を反映

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,18 @@
 class OrdersController < ApplicationController
+  #before_action :authenticate_user!
+  before_action :move_to_sessions, only: :index
+  
+  
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  private
+
+  def move_to_sessions
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+  
 end
