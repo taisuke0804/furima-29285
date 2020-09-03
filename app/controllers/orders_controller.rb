dbecee848959d32000abd0be39ec76c54feea318
taisuke0804 <ultrasoul1987@yahoo.co.jp>
2020-09-03 18:48:43 +0900

    トークン生成コード

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,12 +7,20 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def create
+    
+  end
+
   private
 
   def move_to_sessions
     unless user_signed_in?
       redirect_to root_path
     end
+  end
+
+  def order_params
+    params.permit(:token)
   end
   
 end
