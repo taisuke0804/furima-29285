ffa67ef17d9219515271d1bd87b1d17defb5b4de
taisuke0804 <ultrasoul1987@yahoo.co.jp>
2020-09-08 12:13:11 +0900

    変更４

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'capistrano (3.14.1)'
+lock '3.14.1'
 
 # Capistranoのログの表示に利用する
 set :application, 'furima-29285'
@@ -11,7 +11,7 @@ set :repo_url,  'git@github.com:taisuke0804/furima-29285.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, 'ruby 2.6.5p114' #カリキュラム通りに進めた場合、’2.6.5’ です
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
