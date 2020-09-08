e6a9eccf11fc8e887d9c871f1e91ca0a364c3656
taisuke0804 <ultrasoul1987@yahoo.co.jp>
2020-09-08 22:15:28 +0900

    エラーメッセージ日本語表記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,20}\z/
-    validates :password, format: { with: VALID_PASSWORD_REGEX, message: "半角英数字混合6~20文字で入力してください"}
+    validates :password, format: { with: VALID_PASSWORD_REGEX, message: "を半角英数字混合6~20文字で入力してください"}
   
-  with_options presence: true do
+  with_options presence: true do 
     validates :nickname, length: { maximum: 40 }
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
-    validates :family_name_kata, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください"}
-    validates :first_name_kata, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください"}
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "を全角で入力してください"}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "を全角で入力してください"}
+    validates :family_name_kata, format: { with: /\A[ァ-ヶー－]+\z/, message: "を全角カタカナで入力してください"}
+    validates :first_name_kata, format: { with: /\A[ァ-ヶー－]+\z/, message: "を全角カタカナで入力してください"}
     validates :birth
   end
 
