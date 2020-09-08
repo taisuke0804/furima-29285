14e675643279e8026b87fa39e86511cd8982654a
taisuke0804 <ultrasoul1987@yahoo.co.jp>
2020-09-09 08:57:19 +0900

    購入情報エラー日本語

--- app/forms/credit_order.rb
+++ app/forms/credit_order.rb
@@ -6,11 +6,11 @@ class CreditOrder
   POSTAL_CODE_REGEX = /\d{3}-\d{4}/
 
   with_options presence: true do
-    validates :post_number, format: { with: POSTAL_CODE_REGEX }
-    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :post_number, format: { with: POSTAL_CODE_REGEX, message: "をハイフンありで入力してください" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
     validates :city
     validates :street_number
-    validates :phone_number, format: { with: /\A\d{1,11}\z/ }
+    validates :phone_number, format: { with: /\A\d{1,11}\z/, message: "をハイフンなしで入力してください" }
     validates :token
   end
 
