355f0702699101661676441a1e6d1b9f507c0bd4
taisuke0804 <ultrasoul1987@yahoo.co.jp>
2020-09-09 09:05:51 +0900

    フォームオブジェクト、テストコード修正

--- spec/models/credit_order_spec.rb
+++ spec/models/credit_order_spec.rb
@@ -18,43 +18,43 @@ RSpec.describe CreditOrder, type: :model do
     it 'ハイフンなしの7桁の郵便番号では購入できない' do
       @order.post_number = '1234567'
       @order.valid?
-      expect(@order.errors.full_messages).to include("Post number is invalid")
+      expect(@order.errors.full_messages).to include("郵便番号をハイフンありで入力してください")
     end
 
     it '都道府県の項目を選択してないと購入できない' do
       @order.prefecture_id = 1
       @order.valid?
-      expect(@order.errors.full_messages).to include("Prefecture must be other than 1")
+      expect(@order.errors.full_messages).to include("都道府県を選択してください")
     end
 
     it '市区町村の項目を記入していないと購入できない' do
       @order.city = nil
       @order.valid?
-      expect(@order.errors.full_messages).to include("City can't be blank")
+      expect(@order.errors.full_messages).to include("市町村を入力してください")
     end
 
     it '番地の項目を記入していないと購入できない' do
       @order.street_number = nil
       @order.valid?
-      expect(@order.errors.full_messages).to include("Street number can't be blank")
+      expect(@order.errors.full_messages).to include("番地を入力してください")
     end
 
     it '電話番号の項目を記入してないと購入できない' do
       @order.phone_number = nil
       @order.valid?
-      expect(@order.errors.full_messages).to include("Phone number can't be blank")
+      expect(@order.errors.full_messages).to include("電話番号を入力してください", "電話番号をハイフンなしで入力してください")
     end
 
     it '11桁以上の電話番号では購入できない' do
       @order.phone_number = '090111122223'
       @order.valid?
-      expect(@order.errors.full_messages).to include("Phone number is invalid")
+      expect(@order.errors.full_messages).to include("電話番号をハイフンなしで入力してください")
     end
 
     it 'クレジット情報を正しく入力してないと購入できない' do
       @order.token = nil
       @order.valid?
-      expect(@order.errors.full_messages).to include("Token can't be blank")
+      expect(@order.errors.full_messages).to include("クレジットカード情報を入力してください")
     end
   end
 end
