1fc334eeed589ee358ee5a1c0d442a0ce248dcd4
taisuke0804 <ultrasoul1987@yahoo.co.jp>
2020-09-11 11:07:43 +0900

    詳細な検索

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -1,16 +1,19 @@
 class DetailsController < ApplicationController
+  before_action :search_item, only: [:index, :search]
 
   def index
     @items = Item.all.order("created_at DESC")
+    @item = @p.result.order("created_at DESC").includes(:user)
   end
 
   def search
-    
   end
+
 
   private
 
-  def search_product
-    
+  def search_item
+    @p = Item.ransack(params[:q])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :items do
     resources :orders, only: [:index,:new, :create]
+    #resources :details, only: [:index, :search]
     collection do
       get 'search'
     end
   end
   resources :users, only: [:show, :edit]
-  resources :details, only: [:index, :search]
+  resources :details, only: :index do
+    collection do
+      get 'search'
+    end
+  end
 end
