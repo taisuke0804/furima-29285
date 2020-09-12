d12a9edef1572424d3352ca154cff5f4d8004af1
taisuke0804 <ultrasoul1987@yahoo.co.jp>
2020-09-12 17:38:07 +0900

    commentsコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     collection do
       get 'search'
     end
+    resources :comments, only: :create
   end
   resources :users, only: [:show, :edit]
   resources :details, only: :index do
